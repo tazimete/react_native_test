4bfd8364a228208c7281b23fb7cd822551a83cff
agm-tazim <tazim@kaz-software.com>
2026-04-30 09:32:07 +0600

    Separated data layer

---
+++ app/module/home/data/home.jsx
@@ -0,0 +1,81 @@
+import {Image} from 'react-native';
+
+
+// Sample Data
+const ProjectData = [
+  {
+    index: 0, 
+    icon: (
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{
+          uri:
+            "https://icons.iconarchive.com/icons/limav/flat-gradient-social/256/Twitter-icon.png"
+        }}
+      />
+    ),
+  },
+  {
+    index: 1, 
+    icon: (
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{
+          uri:
+            "https://icons.iconarchive.com/icons/designbolts/free-instagram/256/Active-Instagram-1-icon.png"
+        }}
+      />
+    ),
+  },
+  {
+    index: 2, 
+    icon: (
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{
+          uri:
+            "https://icons.iconarchive.com/icons/limav/flat-gradient-social/256/Twitter-icon.png"
+        }}
+      />
+    ),
+  },
+  {
+    index: 3, 
+    icon: (
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{
+          uri:
+            "https://icons.iconarchive.com/icons/designbolts/free-instagram/256/Active-Instagram-1-icon.png"
+        }}
+      />
+    ),
+  },
+  {
+    index: 4, 
+    icon: (
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{
+          uri:
+            "https://icons.iconarchive.com/icons/limav/flat-gradient-social/256/Twitter-icon.png"
+        }}
+      />
+    ),
+  },
+  {
+    index: 5, 
+    icon: (
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{
+          uri:
+            "https://icons.iconarchive.com/icons/designbolts/free-instagram/256/Active-Instagram-1-icon.png"
+        }}
+      />
+    ),
+  },
+];
+
+
+export default ProjectData
--- app/module/home/view/home.jsx
+++ app/module/home/view/home.jsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import ProjectView from './subview/project_view';
 import HomeStyles from './../style/home';
+import ProjectData from './../data/home';
 
 export default function Home() {
   return (
@@ -11,9 +12,9 @@ export default function Home() {
         <Text>Welcome to NearPro App - Home V2</Text>
         <FlatList
                 contentContainerStyle = {HomeStyles.container}
-                data={itemData}
+                data={ProjectData}
                 numColumns={2}
-                extraData={itemData}
+                extraData={ProjectData}
                 keyExtractor={ (item, index) => index }
                 renderItem={
                     ({item}) => (<ProjectView key={item.index} data={item.icon} />)
@@ -22,79 +23,3 @@ export default function Home() {
     </View>
   );
 }
-
-// Sample Data
-const itemData = [
-  {
-    index: 0, 
-    icon: (
-      <Image
-        style={{ width: 50, height: 50 }}
-        source={{
-          uri:
-            "https://icons.iconarchive.com/icons/limav/flat-gradient-social/256/Twitter-icon.png"
-        }}
-      />
-    ),
-  },
-  {
-    index: 1, 
-    icon: (
-      <Image
-        style={{ width: 50, height: 50 }}
-        source={{
-          uri:
-            "https://icons.iconarchive.com/icons/designbolts/free-instagram/256/Active-Instagram-1-icon.png"
-        }}
-      />
-    ),
-  },
-  {
-    index: 2, 
-    icon: (
-      <Image
-        style={{ width: 50, height: 50 }}
-        source={{
-          uri:
-            "https://icons.iconarchive.com/icons/limav/flat-gradient-social/256/Twitter-icon.png"
-        }}
-      />
-    ),
-  },
-  {
-    index: 3, 
-    icon: (
-      <Image
-        style={{ width: 50, height: 50 }}
-        source={{
-          uri:
-            "https://icons.iconarchive.com/icons/designbolts/free-instagram/256/Active-Instagram-1-icon.png"
-        }}
-      />
-    ),
-  },
-  {
-    index: 4, 
-    icon: (
-      <Image
-        style={{ width: 50, height: 50 }}
-        source={{
-          uri:
-            "https://icons.iconarchive.com/icons/limav/flat-gradient-social/256/Twitter-icon.png"
-        }}
-      />
-    ),
-  },
-  {
-    index: 5, 
-    icon: (
-      <Image
-        style={{ width: 50, height: 50 }}
-        source={{
-          uri:
-            "https://icons.iconarchive.com/icons/designbolts/free-instagram/256/Active-Instagram-1-icon.png"
-        }}
-      />
-    ),
-  },
-];
